370f785612ce3e3843b4d753b4c2ac9fb84fb148
Evan Ahmed <ahmeed.3826@gmail.com>
2020-05-19 23:46:57 +0600

    Riders Option Menu

--- app/src/main/java/com/example/delimanridrnewui/MainActivity.java
+++ app/src/main/java/com/example/delimanridrnewui/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.delimanridrnewui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView=findViewById(R.id.profileImage);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new TestFragment();
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.container, fragment).commit();
+            }
+        });
+
+
     }
 }
